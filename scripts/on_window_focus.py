83cb05b631cb1452c382abfdebc6b134e8f139f3
spacegiraffe <raj.pradeep.kumar@gmail.com>
2026-01-25 09:59:30 +0530

    c*

--- scripts/on_window_focus.py
+++ scripts/on_window_focus.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python3
 import i3ipc
 import subprocess
+from center_mouse import *
 
-
-def on_window_focus(i3, e):
-    # Get the geometry (rect) of the focused container
-    rect = e.container.rect
-
-    # Calculate the absolute center on the screen
-    center_x = rect.x + (rect.width / 2)
-    center_y = rect.y + (rect.height / 2)
-
-    # Move the mouse using absolute screen coordinates
-    subprocess.run(['xdotool', 'mousemove', str(
-        int(center_x)), str(int(center_y))])
-
-    window_id = e.container.window
-    print(f"center_mouse.py: i3 focus changed to window_id: {window_id}")
-
+def on_window_focus(i3, e):    
+    center_mouse()
 
 i3 = i3ipc.Connection()
 i3.on('window::focus', on_window_focus)
